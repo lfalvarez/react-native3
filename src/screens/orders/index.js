1f99e0e46557606cb6f5fa11c9aad30dfae2c34d
Felipe Álvarez <felipe.alvarez@thoughtworks.com>
2022-07-13 20:32:49 -0400

    Adding Image Selector component

--- src/screens/orders/index.js
+++ src/screens/orders/index.js
@@ -1,8 +1,15 @@
+import React, { useState } from "react";
 import { Text, View } from 'react-native';
+import ImageSelector from '../../components/ImageSelector';
 import { styles } from './styles';
 
 export default OrdersScreen = () => {
+    const [image, setImage] = useState("");
+    const onImage = (imageuri) => {
+        setImage(imageuri);
+    };
     return <View style={styles.container}>
-        <Text>OrdersScreen</Text>
+        <Text>Tómale una foto a tu orden</Text>
+        <ImageSelector onImage={onImage} />
     </View>
 }
